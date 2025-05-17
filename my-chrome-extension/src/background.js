d0eb398dfef8afc3ea94f486f2f400299edbf041
Surya <sahuja4@ncsu.edu>
2025-05-17 01:15:07 +0000

    update webpage only on click

--- my-chrome-extension/src/background.js
+++ my-chrome-extension/src/background.js
@@ -1,10 +1,8 @@
 import { transformUrl } from './utils.js';
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && tab.active) {
-        const newUrl = transformUrl(tab.url);
-        if (tab.url !== newUrl) {
-            chrome.tabs.update(tabId, { url: newUrl });
-        }
+chrome.action.onClicked.addListener((tab) => {
+    const newUrl = transformUrl(tab.url);
+    if (tab.url !== newUrl) {
+        chrome.tabs.update(tab.id, { url: newUrl });
     }
 });
