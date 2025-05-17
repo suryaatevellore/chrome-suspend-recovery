f1e9c21ccde8e207d07fe10e5c99bfbff6a104d7
Surya <sahuja4@ncsu.edu>
2025-05-17 01:16:48 +0000

    debug logging to check why the page doesn't reload

--- my-chrome-extension/src/background.js
+++ my-chrome-extension/src/background.js
@@ -1,8 +1,16 @@
 import { transformUrl } from './utils.js';
 
 chrome.action.onClicked.addListener((tab) => {
+    console.log('Extension clicked on tab:', tab);
+    console.log('Original URL:', tab.url);
+    
     const newUrl = transformUrl(tab.url);
+    console.log('Transformed URL:', newUrl);
+    
     if (tab.url !== newUrl) {
+        console.log('Updating tab with new URL');
         chrome.tabs.update(tab.id, { url: newUrl });
+    } else {
+        console.log('URLs match, no update needed');
     }
 });
