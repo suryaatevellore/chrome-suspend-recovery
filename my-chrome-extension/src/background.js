8590b6a1740335116dc9b23572c248b20f3e6e7e
Surya <sahuja4@ncsu.edu>
2025-05-17 01:33:20 +0000

    extension still doesn't work :( more debugs

--- my-chrome-extension/src/background.js
+++ my-chrome-extension/src/background.js
@@ -1,16 +1,23 @@
 import { transformUrl } from './utils.js';
 
-chrome.action.onClicked.addListener((tab) => {
-    console.log('Extension clicked on tab:', tab);
-    console.log('Original URL:', tab.url);
-    
-    const newUrl = transformUrl(tab.url);
-    console.log('Transformed URL:', newUrl);
-    
-    if (tab.url !== newUrl) {
-        console.log('Updating tab with new URL');
-        chrome.tabs.update(tab.id, { url: newUrl });
-    } else {
-        console.log('URLs match, no update needed');
+// Log when service worker loads
+console.log('Service worker initialized');
+
+// Listen for extension button clicks
+chrome.action.onClicked.addListener(async (tab) => {
+    console.log('Button clicked on tab:', tab.id);
+    try {
+        // Request permission for the current tab
+        await chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            func: () => true
+        });
+        
+        const newUrl = transformUrl(tab.url);
+        if (tab.url !== newUrl) {
+            await chrome.tabs.update(tab.id, { url: newUrl });
+        }
+    } catch (error) {
+        console.error('Error:', error);
     }
 });
