d25749ef606f525e7c9f30e42b35f8e84a459b6f
Surya <sahuja4@ncsu.edu>
2025-05-17 01:11:19 +0000

    remove query for all tabs; seems to be crashing chrome

--- my-chrome-extension/src/background.js
+++ my-chrome-extension/src/background.js
@@ -2,14 +2,9 @@ import { transformUrl } from './utils.js';
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === 'complete' && tab.active) {
-        chrome.tabs.query({}, (tabs) => {
-            tabs.forEach((currentTab) => {
-                const newUrl = transformUrl(currentTab.url);
-                if (currentTab.url !== newUrl) {
-                    // Update the tab URL if it has changed
-                    chrome.tabs.update(currentTab.id, { url: newUrl });
-                }
-            });
-        });
+        const newUrl = transformUrl(tab.url);
+        if (tab.url !== newUrl) {
+            chrome.tabs.update(tabId, { url: newUrl });
+        }
     }
 });
